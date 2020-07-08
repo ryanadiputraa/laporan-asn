e879d5fc423cfa28658ecf5e429c790701428754
ryan <ryannadiputraa@gmail.com>
2020-07-08 09:44:06 +0800

    created hamburger animations, fixed page padding

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,19 +9,33 @@ const Navbar = () => {
     navbar.classList.toggle('active-navbar');
   }
 
+  const toggleHam = () => {
+    const ham = document.querySelectorAll('.ham');
+    ham.forEach(h => h.classList.toggle('active-ham'))
+  }
+
   return (
     <div className="navbar">
       <div className="main-link">
         <NavLink className="title" exact to="/">LAPORAN ASN</NavLink>
       </div>
-      <div className="hamburger" onClick={toggleNavbar}>
+      <div className="hamburger" onClick={() => {
+        toggleNavbar();
+        toggleHam();
+      }}>
         <div className="ham"></div>
         <div className="ham"></div>
         <div className="ham"></div>
       </div>
       <div className="nav-links">
-        <NavLink className="nav-link" to="/about">Tentang</NavLink>
-        <NavLink className="nav-link" to="/help">Bantuan</NavLink>
+        <NavLink className="nav-link" to="/about" onClick={() => {
+          toggleNavbar();
+          toggleHam();
+        }}>Tentang</NavLink>
+        <NavLink className="nav-link" to="/help" onClick={() => {
+          toggleNavbar();
+          toggleHam();
+        }}>Bantuan</NavLink>
       </div>
     </div>
   )
--- src/containers/Pages/About/About.js
+++ src/containers/Pages/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className="about container">
       <h2 className="text-center mt-4">Aplikasi Laporan ASN</h2>
-      <p className="about-text mt-4 text-justify">Aplikasi ini dibuat untuk Aparatur Sipil Negara (ASN) dalam pembuatan laporan kegiatan harian. Aplikasi berbasis web menerima dan mengelola input yang kemudian diubah dalam bentuk pdf, dengan format tabel yang telah disesuaikan. Untuk panduan penggunaan dapat dilihat pada menu bantuan. Klik icon sosial media di bawah untuk membagikan link web ini.</p>
+      <p className="about-text mt-4 text-justify">Aplikasi ini dibuat untuk Aparatur Sipil Negara (ASN) dalam pembuatan laporan kegiatan harian. Aplikasi berbasis web menerima dan mengelola input yang kemudian diubah dalam bentuk pdf, dengan format tabel yang telah disesuaikan. Untuk panduan penggunaan dapat dilihat pada menu bantuan. Klik icon sosial media di bawah untuk membagikan aplikasi</p>
     </div>
   )
 }
