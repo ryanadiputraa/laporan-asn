142db035abf96f565a8673cd58f43ef4ab7a9e54
ryanadiputraa <ryannadiputraa@gmail.com>
2020-12-15 21:58:52 +0800

    created register route and hash password

--- api/models/asn.js
+++ api/models/asn.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 const asnSchema = mongoose.Schema({
-    nip: { type: Number, required: true, unique: true },
-    email: { type: String, required: true },
+    nip: { type: Number, required: true, unique: true, min: 18 },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+     },
     password: { type: String, required: true },
     name: { type: String, required: true },
     pos: { type: String, required: true },
--- api/routes/asn.js
+++ api/routes/asn.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 // ASN Schema
 const ASN = require('../models/asn');
@@ -36,21 +37,58 @@ router.get('/login', (req, res, next) => {
 
 // Register data
 router.post('/register', (req, res, next) => {
-    const asn = new ASN({
-        nip: req.body.nip,
-        email: req.body.email,
-        password: req.body.password,
-        name: req.body.name,
-        pos: req.body.pos,
-        bossName: req.body.bossName,
-        bossPos: req.body.bossPos,
-        city: req.body.city
-    });
-    asn.save()
-        .then(result => {
-            console.log(result);
-            res.status(201).json({
-                code: 201,
+    ASN.find({ nip: req.body.nip }).exec()
+        .then(asn => {
+            if(asn.length >= 1) {
+                return res.status(409).json({
+                    code: 409,
+                    status: 'not ok'
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if(err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const asn = new ASN({
+                            nip: req.body.nip,
+                            email: req.body.email,
+                            password: hash,
+                            name: req.body.name,
+                            pos: req.body.pos,
+                            bossName: req.body.bossName,
+                            bossPos: req.body.bossPos,
+                            city: req.body.city
+                        });
+                        asn.save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    code: 201,
+                                    status: 'ok'
+                                })
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    code: 500,
+                                    status: 'not ok',
+                                    error: err
+                                });
+                            });
+                    }
+                })
+            }
+        })
+});
+
+router.delete('/delete/:nip', (req, res, next) => {
+    ASN.remove({ nip: req.params.nip })
+        .exec()
+        .then(() => {
+            res.status(200).json({
+                code: 200,
                 status: 'ok'
             })
         })
@@ -60,8 +98,8 @@ router.post('/register', (req, res, next) => {
                 code: 500,
                 status: 'not ok',
                 error: err
-            });
-        });
+            })
+        })
 });
 
 
